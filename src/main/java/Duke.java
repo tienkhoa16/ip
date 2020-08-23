181445acc2bf63209be10b681385ea75810e765d
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-08-23 22:26:56 +0800

    Add greeting and farewell

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
 public class Duke {
+    public static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    /**
+     * Greet user.
+     */
+    public static void printHello() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" How can I help you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    /**
+     * Bye user.
+     */
+    public static void printBye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHello();   // Greet user
+        printBye();     // Bye user
     }
 }
