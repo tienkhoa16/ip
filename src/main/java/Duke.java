c80eee8ecb18f132c52aea576539ea98f8697a83
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-08-23 22:41:54 +0800

    Add replyCommand, addTask, listTasks methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Duke {
     public static final String HORIZONTAL_LINE = "\t____________________________________________________________";
+    public static String[] tasks = new String[100]; // Store user's tasks
+    public static int taskNumber = 0;               // Manage number of tasks added
 
     /**
      * Greet user.
@@ -49,9 +51,59 @@ public class Duke {
         }
     }
 
+    /**
+     * Replies user's command.
+     * If the command is "bye", exits method.
+     * Otherwise, replies user accordingly to the command.
+     */
+    public static void replyCommand() {
+        String command = getCommand();  // Get user's command
+
+        // Repeat until command is "bye"
+        while (!command.equals("bye")) {
+            switch (command) {
+            case "list":    // If command is "list", list tasks added so far
+                listTasks();
+                break;
+            default:        // By default, add user's command as a new task to tasks array
+                addTask(command);
+                break;
+            }
+
+            command = getCommand(); // Get user's following command
+        }
+    }
+
+    /**
+     * Adds a new task to tasks array.
+     * After the task is added, notifies user.
+     *
+     * @param task
+     */
+    public static void addTask(String task) {
+        tasks[taskNumber] = task;
+        taskNumber++;
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t added: " + task); //  Notify user that the task is recorded
+        System.out.println(HORIZONTAL_LINE + "\n");
+    }
+
+    /**
+     * Lists out tasks added so far with their status.
+     */
+    public static void listTasks() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t Here are the tasks in your list:");
+        for (int i = 0; i < taskNumber; i++) {
+            System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+        }
+        System.out.println(HORIZONTAL_LINE + "\n");
+    }
+
     public static void main(String[] args) {
         printHello();   // Greet user
-        echoCommand();  // Get user's command and echo
+        replyCommand(); // Get user's command and reply properly
         printBye();     // Bye user
     }
 }
