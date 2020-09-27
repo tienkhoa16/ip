d3eb75ab19aa1524ab635ca45a23058dbe94ea9a
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-09-27 21:41:03 +0800

    Add FindCommand class to find tasks by keyword

---
+++ src/main/java/duke/exceptions/EmptyKeywordException.java
@@ -0,0 +1,12 @@
+package duke.exceptions;
+
+import static duke.constants.Messages.MESSAGE_EMPTY_KEYWORD;
+
+/**
+ * Represents exception when keyword parsed from user's input is empty.
+ */
+public class EmptyKeywordException extends DukeException {
+    public EmptyKeywordException() {
+        super(String.format(MESSAGE_EMPTY_KEYWORD));
+    }
+}
